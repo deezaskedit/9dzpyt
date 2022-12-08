24cf6ad2e2a3ca1b0f26d3ae140e8f98f4dacf64
Artem <aneday05@mail.ru>
2022-12-09 02:02:26 +0300

    done

---
+++ main.py
@@ -0,0 +1,12 @@
+import controller as c
+
+# для вызова - анкоммент
+
+# 1. Вызов функции экспорта
+c.add_records_in_file_print_with_textable()
+
+# 2. Вызов функции импорта
+#c.take_records_from_file_with_textable()
+
+# 3. Вызов функции добавления записей
+#c.add_records_in_file_with_available_records_with_textable()
